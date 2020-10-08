283804f3024904f44b5470f67caab861f84e4194
lolipopluxury <songyupeng@yahoo.com>
2020-10-08 21:39:28 +0800

    set up for navigation

--- App.js
+++ App.js
@@ -1,18 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import React,{useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-// import { Button, Divider } from 'react-native-elements';
-import { Button, WhiteSpace, WingBlank } from '@ant-design/react-native';
+import * as Font from 'expo-font';
+import { AppLoading} from 'expo';
 
-export default function App() {
+
+import { Button, WhiteSpace, WingBlank, Icon, SearchBar, TabBar } from '@ant-design/react-native';
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button type="primary">primary</Button>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+        selectedTab: 'home',
+        isReady: false,
+    }
+  }  
+
+  async componentDidMount() {
+    await Font.loadAsync(
+        'antoutline',        
+        require('@ant-design/icons-react-native/fonts/antoutline.ttf')
+    );
+    await Font.loadAsync(
+        'antfill',        
+        require('@ant-design/icons-react-native/fonts/antfill.ttf')
+    );    
+    this.setState({ isReady: true });
+ } 
+
+  onButtonClick = () => {
+    console.log(this.selectedTab)
+  }
+  render(){
+    const {isReady } = this.state;
+    if (!isReady) {
+        return <AppLoading />;
+    }
+    return (
+      <NavigationContainer>    
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer> 
+    );
+  }  
 }
 
 const styles = StyleSheet.create({
@@ -23,3 +77,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
---
+++ pages/Mine.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Mine extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>Mine page</Text>
+      </View>
+    )
+  }
+}
